afb567ab99e2cde0c9fb0320ea3da42ff6a0260a
subeet <subeetdutta@gmail.com>
2020-08-18 21:52:38 +0530

    added admin for models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+# Todo Not Completed
+
+# Define admin class
+class AuthorAdmin(admin.ModelAdmin):
+    """description"""
+    pass
+admin.site.register(Author,AuthorAdmin)
+
+admin.site.register(Genre)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
