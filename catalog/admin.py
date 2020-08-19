1981cc9397d0f7c756ec9f0403ef349550cd2c8b
subeet <subeetdutta@gmail.com>
2020-08-19 14:08:11 +0530

    added admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,16 +3,21 @@ from .models import Author, Genre, Book, BookInstance
 
 # Todo Not Completed
 
-# Define admin class
-class AuthorAdmin(admin.ModelAdmin):
-    """description"""
-    pass
-admin.site.register(Author,AuthorAdmin)
 
-admin.site.register(Genre)
+@admin.register(Author)
+class AuthorAdmin(Author.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(Genre.ModelAdmin):
+    pass
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
